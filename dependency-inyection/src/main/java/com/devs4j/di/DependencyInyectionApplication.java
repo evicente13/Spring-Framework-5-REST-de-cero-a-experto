e484371fd1408491f22dfadca9b0182f2d9c7bbf
Eduardo <eduardoartemis@gmail.com>
2023-09-22 15:00:12 -0500

    Video 08 - 22/09/2023 15:00

---
+++ dependency-inyection/src/main/java/com/devs4j/di/DependencyInyectionApplication.java
@@ -0,0 +1,29 @@
+package com.devs4j.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.devs4j.di.atributo.Coche;
+
+@SpringBootApplication
+public class DependencyInyectionApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(DependencyInyectionApplication.class, args);
+		
+		//Motor motor = new Motor("Xl1", 1981);
+		//Coche coche = new Coche("VW", 1986, motor);
+		
+		//System.out.println(coche);
+		
+		//Inyeccion de Dependencias por atributo
+		
+		ConfigurableApplicationContext context =  SpringApplication.run(DependencyInyectionApplication.class, args);
+		
+		Coche coche = context.getBean(Coche.class);
+		
+		System.out.println(coche);
+	}
+
+}
