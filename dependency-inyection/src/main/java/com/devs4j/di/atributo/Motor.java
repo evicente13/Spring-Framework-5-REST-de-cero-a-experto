015a701b4cf4cca6377a633b871fbe28d19fe158
Eduardo Vicente <evicente.ti@gmail.com>
2023-09-23 00:03:05 -0500

    Video 09 - 23/09/2023 00:03

--- dependency-inyection/src/main/java/com/devs4j/di/atributo/Motor.java
+++ dependency-inyection/src/main/java/com/devs4j/di/atributo/Motor.java
@@ -13,16 +13,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class Motor {
 	
-	@Value("XL1")
 	private String marca;
-	
-	@Value("1984")
 	private Integer modelo;
 	
-	public Motor() {
-	}
-
-	public Motor(String marca, Integer modelo) {
+	public Motor(@Value("XL1") String marca, @Value("1984") Integer modelo) {
 		this.marca = marca;
 		this.modelo = modelo;
 	}
