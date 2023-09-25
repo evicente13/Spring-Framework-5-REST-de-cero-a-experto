db3b64b811780be50f34fde5bf4fa088869c644d
Eduardo <eduardoartemis@gmail.com>
2023-09-25 12:49:07 -0500

    Video 16 - 25/09/2023 12:49

--- dependency-inyection/src/main/java/com/devs4j/di/DependencyInyectionApplication.java
+++ dependency-inyection/src/main/java/com/devs4j/di/DependencyInyectionApplication.java
@@ -12,6 +12,7 @@ import com.devs4j.di.qualifiers.Avion;
 import com.devs4j.di.qualifiers.Nido;
 import com.devs4j.di.qualifiers.Pajaro;
 import com.devs4j.di.qualifiers.Perro;
+import com.devs4j.diprofiles.EnviromentService;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
@@ -30,9 +31,6 @@ public class DependencyInyectionApplication {
 		
 		//Inyeccion de Dependencias por atributo
 		
-		
-		ConfigurableApplicationContext context =  SpringApplication.run(DependencyInyectionApplication.class, args);
-		
 		//Perro perro = context.getBean(Perro.class);
 		//Pajaro pajaro = context.getBean(Pajaro.class);
 		//Avion avion = context.getBean(Avion.class);
@@ -40,9 +38,15 @@ public class DependencyInyectionApplication {
 		//Animal animal = context.getBean("perro", Animal.class);
 		//log.info("Animal nombre = {} edad = {}", animal.getNombre(), animal.getEdad());
 		
+		//Nido nido = context.getBean(Nido.class);
+		//nido.imprimir();
 		
-		Nido nido = context.getBean(Nido.class);
-		nido.imprimir();
+		ConfigurableApplicationContext context =  SpringApplication.run(DependencyInyectionApplication.class, args);
+		
+		EnviromentService enviromentService = context.getBean(EnviromentService.class);
+		
+		log.info("Active enviroment {}", enviromentService.getEnviroment());
+		
 		
 	}
 
