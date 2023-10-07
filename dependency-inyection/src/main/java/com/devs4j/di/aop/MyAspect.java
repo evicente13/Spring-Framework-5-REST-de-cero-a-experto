ee039578dde87d18cdf03f84ee6d4bb4661248b3
Eduardo Vicente <evicente.ti@gmail.com>
2023-10-07 16:14:49 -0500

    video 35 - 07/10/2023 16:14

--- dependency-inyection/src/main/java/com/devs4j/di/aop/MyAspect.java
+++ dependency-inyection/src/main/java/com/devs4j/di/aop/MyAspect.java
@@ -1,5 +1,6 @@
 package com.devs4j.di.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -14,9 +15,11 @@ public class MyAspect {
 	private static final Logger log = LoggerFactory.getLogger(MyAspect.class);
 
 	@Before("execution(* com.devs4j.di.aop.TargetObject.*(..))")
-	public void before() {
+	public void before(JoinPoint joinPoint) {
+		log.info("Method name {} ", joinPoint.getSignature().getName() );
+		log.info("Object type {} ", joinPoint.getSignature().getDeclaringTypeName() );
+		log.info("Modifiers {} ", joinPoint.getSignature().getModifiers() );
+		log.info("Arguments {} ", joinPoint.getArgs() );
 		log.info("Before Advice");
 	}
-	
-	
 }
