eb527a6ec3fdbd52eadb0e88bd0aaa0850e2801d
Eduardo Vicente <evicente.ti@gmail.com>
2023-10-07 22:40:00 -0500

    Video 38 - 07/10/2023 23:29

--- dependency-inyection/src/main/java/com/devs4j/di/aop/PointcutExample.java
+++ dependency-inyection/src/main/java/com/devs4j/di/aop/PointcutExample.java
@@ -4,7 +4,8 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class PointcutExample {
 
-    @Pointcut("execution(* com.devs4j.di.aop.TargetObject.*(..))")
+    //@Pointcut("within(com.devs4j.di.aop.*)")
+    @Pointcut("@annotation(Devs4jAnnotation)")
     public void targetObjectMethods(){
 
     }
